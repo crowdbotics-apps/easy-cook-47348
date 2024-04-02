ccd7d6f69fc8e4407e00f0228ec7ce87b0512f4e
Crowdbotics <team@crowdbotics.com>
2024-04-02 10:58:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "easy-cook-47348",
+  url: "https://easy-cook-47348.botics.co",
+  api: "https://easy-cook-47348.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
